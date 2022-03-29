67caa3a44eadca3222b6927cb95652200321befd
Mykyta Prus <mike.prus@ekreative.com>
2022-03-29 10:10:11 +0300

    feat: fetch posts from backend

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,27 +1,63 @@
 import type { NextPage } from 'next';
+import { gql } from '@apollo/client';
 
 import Layout from '@/components/Layout';
-import { getPosts } from '@/lib/posts';
-import { PostsProps } from '@/models/post.props';
 import Post from '@/components/Post';
 
-const BlogPage: NextPage<PostsProps> = ({ posts }) => {
+import { PostsProps } from '@/models/post.props';
+
+import client from '@/lib/apollo-client';
+
+const GetPostsQuery = gql`
+    query {
+        articles {
+            data {
+                attributes {
+                    title
+                    excerpt
+                    slug
+                    publicationDate
+                    categories {
+                        data {
+                            attributes {
+                                name
+                            }
+                        }
+                    }
+                    cover {
+                        data {
+                            attributes {
+                                formats
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const BlogPage: NextPage<{ data: PostsProps }> = ({ data }) => {
     return (
         <Layout>
             <h2 className="text-4xl font-bold mb-4 dark:text-gray-200">All posts</h2>
-            {posts.map((post, index) => (
-                <Post key={index} slug={post.slug} frontmatter={post.frontmatter} />
-            ))}
+            {data.articles.data.map((article) => {
+                const { attributes } = article;
+
+                return <Post attributes={attributes} key={attributes?.slug} />;
+            })}
         </Layout>
     );
 };
 
-export function getStaticProps() {
-    const posts = getPosts();
+export async function getStaticProps() {
+    const { data } = await client.query({
+        query: GetPostsQuery,
+    });
 
     return {
         props: {
-            posts,
+            data,
         },
     };
 }
