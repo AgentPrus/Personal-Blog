f3f05ea08d55e634c6be34ee1b244c484a525414
Mykyta Prus <mike.prus@ekreative.com>
2022-04-08 09:24:06 +0300

    refactor: custom image component

--- components/CustomImage/CustomImage.tsx
+++ components/CustomImage/CustomImage.tsx
@@ -2,17 +2,23 @@ import Image from 'next/image';
 import { useWindowSize } from 'react-use';
 
 import { parseImageFormats } from '@/utils/image';
-import { UploadFileEntity } from 'generated/graphql-types';
+import { Maybe, UploadFileEntity } from 'generated/graphql-types';
 
-const CustomImage: React.FC<UploadFileEntity> = ({ attributes }) => {
+const CustomImage: React.FC<{
+    imageProps?: Maybe<UploadFileEntity>;
+    className?: string;
+    imgWidth?: number;
+    imgHeight?: number;
+}> = ({ imageProps, className = '', imgWidth, imgHeight }) => {
     const { width } = useWindowSize();
-    const imageData = parseImageFormats(width, attributes?.formats);
+    const imageData = parseImageFormats(width, imageProps?.attributes?.formats);
 
     return (
         <Image
+            className={className}
             src={imageData?.url ?? '/images/default-fallback-image.png'}
-            width={imageData?.width}
-            height={imageData?.height}
+            width={imgWidth ?? imageData.width}
+            height={imgHeight ?? imageData.height}
             alt={imageData?.name}
         />
     );
