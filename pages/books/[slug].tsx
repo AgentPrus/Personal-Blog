94e2235a0b042888abb91ca456385bb83e254732
Mykyta Prus <mike.prus@ekreative.com>
2022-04-12 10:56:52 +0300

    feat: add book review content page

--- pages/books/[slug].tsx
+++ pages/books/[slug].tsx
@@ -1,6 +1,87 @@
+import BookReviewContent from '@/components/Books/BookReviewContent';
 import Layout from '@/components/Layout';
-import { NextPage } from 'next';
+import client from '@/lib/apollo-client';
+import { gql } from '@apollo/client';
+import { BookReviewEntity, BookReviewEntityResponseCollection } from 'generated/graphql-types';
+import { GetStaticPaths, NextPage } from 'next';
+import { useRouter } from 'next/router';
 
-const BookReviewPage: NextPage = () => {
-    return <Layout></Layout>;
+const GetBooksReview = gql`
+    query {
+        bookReviews {
+            data {
+                attributes {
+                    slug
+                }
+            }
+        }
+    }
+`;
+
+const GetBookReviewBySlug = gql`
+    query BookReviews($slug: String!) {
+        bookReviews(filters: { slug: { eq: $slug } }) {
+            data {
+                attributes {
+                    slug
+                    title
+                    description
+                    cover {
+                        data {
+                            attributes {
+                                formats
+                            }
+                        }
+                    }
+                    createdAt
+                }
+            }
+        }
+    }
+`;
+
+const BookReviewPage: NextPage<BookReviewEntityResponseCollection> = ({
+    data,
+}: BookReviewEntityResponseCollection) => {
+    const router = useRouter();
+
+    return router.isFallback ? (
+        <div>Loading...</div>
+    ) : (
+        <Layout title={data?.[0]?.attributes?.title}>
+            <BookReviewContent attributes={data?.[0]?.attributes} />
+        </Layout>
+    );
 };
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await client.query({
+        query: GetBooksReview,
+    });
+
+    const paths = data?.['bookReviews']?.data.map(({ attributes }: BookReviewEntity) => ({
+        params: {
+            slug: attributes?.slug,
+        },
+    }));
+
+    return {
+        paths,
+        fallback: true,
+    };
+};
+
+export const getStaticProps = async ({ params: { slug } }: { params: { slug: string } }) => {
+    const { data } = await client.query({
+        query: GetBookReviewBySlug,
+        variables: { slug },
+    });
+
+    return {
+        props: {
+            data: data?.bookReviews.data,
+        },
+    };
+};
+
+export default BookReviewPage;
