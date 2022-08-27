6592aa6840c80a655949c2f9a824643b3ff85f7d
Mykyta Prus <mike.prus@ekreative.com>
2022-08-27 18:03:06 +0300

    feat: update posts looks

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -2,31 +2,47 @@ import Link from 'next/link';
 import { ArticleEntity } from 'generated/graphql-types';
 import dayjs from 'dayjs';
 import Categories from '../Categories';
+import { RiCheckboxBlankCircleLine } from 'react-icons/ri';
+import { MdOutlineNavigateNext } from 'react-icons/md';
+import { useRouter } from 'next/router';
 
 const Post: React.FC<ArticleEntity> = ({ attributes }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/blog/${attributes?.slug}`);
+    };
+
     return (
-        <div className="w-full mb-8">
-            <Link href={`/blog/${attributes?.slug}`}>
-                <a>
-                    <h3 className="mb-2 font-bold text-xl text-slate-900 dark:text-slate-200">
-                        {attributes?.title}
-                    </h3>
-                    <div className="flex justify-between items-center mb-2">
-                        <p className="italic text-black-slate-900 dark:text-slate-200">
-                            {dayjs(attributes?.publicationDate).format('MMMM D, YYYY')}
-                        </p>
-                        {attributes?.categories?.data && (
-                            <Categories data={attributes?.categories?.data} />
-                        )}
-                    </div>
-                    <div>
-                        <p className="text-lg text-slate-900 dark:text-slate-200">
-                            {attributes?.excerpt}
-                        </p>
-                    </div>
-                </a>
-            </Link>
-        </div>
+        <article className="relative group cursor-pointer z-10" onClick={handleClick}>
+            <RiCheckboxBlankCircleLine className="hidden absolute right-full mr-6 top-2 text-slate-200 dark:text-slate-600 md:mr-11 overflow-visible sm:block" />
+            <div className="absolute -inset-y-2.5 -inset-x-4 md:-inset-y-4 md:-inset-x-6 sm:rounded-2xl group-hover:bg-slate-50/50 dark:group-hover:bg-slate-800/25" />
+            <div className="relative w-10/12">
+                <h3 className="mb-2 font-bold text-xl text-slate-900 dark:text-slate-200">
+                    {attributes?.title}
+                </h3>
+                <div className="flex justify-between items-center mb-2">
+                    <p className="italic text-black-slate-900 dark:text-slate-200">
+                        {dayjs(attributes?.publicationDate).format('MMMM D, YYYY')}
+                    </p>
+                    {attributes?.categories?.data && (
+                        <Categories data={attributes?.categories?.data} />
+                    )}
+                </div>
+                <div>
+                    <p className="text-lg text-slate-900 dark:text-slate-200">
+                        {attributes?.excerpt}
+                    </p>
+                </div>
+
+                <Link href={`/blog/${attributes?.slug}`}>
+                    <a className="flex items-center text-sky-500 dark:text-sky-400">
+                        Read more
+                        <MdOutlineNavigateNext className="text-sky-500 dark:text-sky-400" />
+                    </a>
+                </Link>
+            </div>
+        </article>
     );
 };
 
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -12,9 +12,12 @@ const BlogPage: NextPage<{ data: PostsProps }> = ({ data }) => {
     return (
         <Layout>
             <h2 className="text-2xl md:text-4xl font-bold mb-4 dark:text-gray-200">All posts</h2>
-            {data.articles.data.map(({ attributes }) => (
-                <Post attributes={attributes} key={attributes?.slug} />
-            ))}
+            <div className="relative space-y-8">
+                <div className="hidden absolute top-12 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800 sm:block" />
+                {data.articles.data.map(({ attributes }) => (
+                    <Post attributes={attributes} key={attributes?.slug} />
+                ))}
+            </div>
         </Layout>
     );
 };
